982aeba4d10afaa6468cc51ad973eace86eefaee
Erudite247 <eruditesolomon@gmail.com>
2024-06-30 22:57:27 +0100

    updated

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,3 +1,4 @@
+#!/bash/bin
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operator = input("Choose the operation (+, -, *, /): ")
---
+++ python_introduction/Mad _Libs _story .py
@@ -0,0 +1,13 @@
+
+
+noun = input("Enter a word: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a word('Verb'): ")
+
+story = f"""On a beautiful {adjective} day I went to the zoo. clearI saw a funny {noun} monkey swinging from the trees.
+       Then, I spotted a majestic {adjective} lion lounging in the sun.
+       What a wild and {verb} experience!"""
+print(story)
+
+
+
