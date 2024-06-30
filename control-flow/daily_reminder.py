6d10b967e9e2ac00025fec5bcdb9f40db9506829
Erudite247 <eruditesolomon@gmail.com>
2024-06-30 23:35:21 +0100

    This script will ask the user for a single task, its priority level, and if it is time-sensitive.

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,18 @@
+#!/bash/bin
+task = input("Enter your task: ")
+priority = input("Priority(high/medium/low): ")
+time_bound = input("Is it time-bound? (Yes/no): ")
+
+match priority:
+    case "high":
+        if time_bound.lower() == "yes":
+            reminder = (f"{task} is a high priority task that requires immediate attention today!")
+        else:
+            reminder =  (f"{task} is a high priority task")
+
+    case "medium":
+        reminder = (f"{task} is a medium priority task")
+    case "low":
+        if time_bound.lower() == "yes":
+            reminder = (f"{task} is a low priority task. Consider completing it when you have free time.")
+print("Reminder:", reminder)
