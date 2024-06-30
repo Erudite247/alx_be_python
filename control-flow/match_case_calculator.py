7da27d730c4599d884fdf7c7b5ca670baa330445
Erudite247 <eruditesolomon@gmail.com>
2024-06-30 23:08:05 +0100

    updated

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,7 +3,6 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operator = input("Choose the operation (+, -, *, /): ")
 
-
 match operator:
     case "+":
         result = num1 + num2
